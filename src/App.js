c71c67ae250d85edb3d6df5e3e75bc406460f1a0
tammenterho <terho.mikko@gmail.com>
2023-06-02 13:25:50 +0300

    add item feature

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import { useState } from "react";
 
 export default function App() {
 
-  const [newItem, setNewItem] = useState("Moi")
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+function handleSubmit(e) {
+
+  setTodos(currentTodos => {
+    return [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title: newItem, completed: false },
+    ]
+  })
+}
 
   return (
     <>
-    <form className="esine-form">
+    <form 
+    onSubmit={handleSubmit}
+    className="esine-form">
       <label>
         Uusi esine
       </label>
       <input
+      onChange={e => setNewItem(e.target.value)}
       value={newItem}
       type="text"
       id="esine"
       />
       <button>lisää</button>
+      <button>anna</button>
     </form>
     <h1>Lista</h1>
       <ul>
