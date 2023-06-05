4b396582aede2beeb750bece68eb9f15c3e2433b
tammenterho <terho.mikko@gmail.com>
2023-06-05 15:18:37 +0300

    basic logic for todo

--- src/App.js
+++ src/App.js
@@ -5,39 +5,73 @@ export default function App() {
   const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
 
-function handleSubmit(e) {
-
-  setTodos(currentTodos => {
-    return [
-      ...currentTodos,
-      { id: crypto.randomUUID(), title: newItem, completed: false },
-    ]
-  })
-}
+  
+  function handleSubmit(e) {
+    e.preventDefault()
+    
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+    
+    setNewItem("")
+  }
+  console.log(todos)
+  
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+  
+  function handleDelete(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
-    <form 
-    onSubmit={handleSubmit}
-    className="esine-form">
-      <label>
-        Uusi esine
-      </label>
-      <input
-      onChange={e => setNewItem(e.target.value)}
-      value={newItem}
-      type="text"
-      id="esine"
-      />
-      <button>lisää</button>
-      <button>anna</button>
-    </form>
-    <h1>Lista</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="esine-form">
+        <label>
+          New Item
+        </label>
+        <input
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+          type="text"
+          id="esine"
+        />
+        <button>lisää</button>
+      </form>
+      <h1>List</h1>
       <ul>
-        <li>
-          esine1
-        </li>
+        {todos.map(todo => {
+          return (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" 
+              checked={todo.completed}
+              onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+            {todo.title}
+            <button 
+            className="btn-delete" 
+            onClick={() => handleDelete(todo.id)}
+            >Delete</button>
+            </label>
+          </li>
+        )
+        })}
       </ul>
-      </>
+    </>
   );
 }
