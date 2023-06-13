3b7afa8dd0c2f12580915fc7b542ce7b76b96cfd
tammenterho <terho.mikko@gmail.com>
2023-06-13 21:39:17 +0300

    success alert

--- src/newTodoForm.jsx
+++ src/newTodoForm.jsx
@@ -1,25 +1,39 @@
 import { useState } from "react"
 import { Button, TextField } from "@mui/material"
+import Alert from '@mui/material/Alert';
 import './../src/styles.css'
 
 
 export function NewTodoForm(props) {
 
     const [newItem, setNewItem] = useState("")
+    const [successVisible, setSuccessVisible] = useState(false)
     
 
     function handleSubmit(e) {
         e.preventDefault()
 
         props.onSubmit(newItem)
+        setSuccessVisible(true)
+
+        setTimeout(() => {
+            setSuccessVisible(false)
+        }, 5000)
 
         setNewItem("")
     }
 
     return (
+        <>
+        {successVisible && (
+            <Alert severity="success">
+                New todo added succesfully!
+            </Alert>
+        )}
         <form
             onSubmit={handleSubmit}
             className="item-form">
+
             
             <TextField
                 value={newItem}
@@ -36,6 +50,6 @@ export function NewTodoForm(props) {
             type="submit">
                 Add</Button> 
         </form>
-
+        </>
     )
 }
